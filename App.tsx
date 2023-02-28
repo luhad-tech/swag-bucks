4e31f42329eb9a445e747b791db12bab91706441
Luke Harding <lukeharding990@gmail.com>
2023-02-28 03:03:39 -0500

    SafeArea for iOS and Android, Header, Placeholder

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useSafeAreaInsets,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+    <SafeAreaProvider>
+      <MainView />
+    </SafeAreaProvider>
+  );
+}
+
+function MainView() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Text style={[styles.heading]}>
+        Luhad Internal Codename: Swag Bucks{"\n"}
+        <Text style={{ color: "red" }}>EXPERIMENTAL VERSION</Text>
+      </Text>
+      <View style={[styles.body]}>
+        <Text style={{ fontSize: 25, fontWeight: "bold" }}>{"<SomethingGoesHere/>"}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,12 +32,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    //justifyContent: "center",
   },
   heading: {
-    fontSize: 25,
+    fontSize: 20,
     textAlign: "center",
-  }
+  },
+  body: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
