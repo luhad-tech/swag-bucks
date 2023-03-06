7644fb67a44fb035abeb8effe2cbd906d30262c9
lukeh990 <lukeharding990@gmail.com>
2023-03-06 12:52:52 -0500

    oauth part 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { client } from "../utils/pocketbase";
 import { stylesheet } from "../utils/stylesheet";
 import { useEffect, useState } from "react";
+import { AuthProviderInfo } from "pocketbase";
+
+const loginOAuth = async (provider: AuthProviderInfo) => {
+  try {
+    const res = await client
+      .collection("users")
+      .authWithOAuth2(
+        provider.name,
+        provider.codeChallenge,
+        provider.codeVerifier,
+        provider.authUrl
+      );
+
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default function Page() {
-  const [data, setData] = useState<string>();
+  const [data, setData] = useState<AuthProviderInfo[]>();
   useEffect(() => {
     (async () => {
       try {
         const results = await client.collection("users").listAuthMethods();
-        setData(JSON.stringify(results.authProviders));
+        setData(results.authProviders);
       } catch (err) {
         console.log(err);
         setData(err);
@@ -22,7 +40,23 @@ export default function Page() {
     <View style={stylesheet.container}>
       <View style={stylesheet.main}>
         <Text style={stylesheet.title}>Hello World</Text>
-        <Text style={stylesheet.subtitle}>{client.baseUrl}</Text>
+        <View style={stylesheet.subtitle}>
+          {data ? (
+            data.map((provider) => {
+              const name =
+                provider.name.charAt(0).toUpperCase() + provider.name.slice(1);
+              return (
+                <Button
+                  onPress={() => loginOAuth(provider)}
+                  key={name}
+                  title={name}
+                />
+              );
+            })
+          ) : (
+            <Text>Loading...</Text>
+          )}
+        </View>
       </View>
     </View>
   );
